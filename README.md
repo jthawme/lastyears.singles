8cd5411e81d73b5d01cb637dc1ee8169b70e8dc7
Jonny Thaw <hi@jthaw.me>
2023-12-07 13:45:38 +0000

    feat: added first playlist of 2023

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Last Years Singles
 
-Sorry I haven't commited up the code yet, I hope to do a big write up in the new year
+Sorry I haven't commented up the code yet, I hope to do a big write up in the new year
--- scripts/sources/mine.js
+++ scripts/sources/mine.js
@@ -7,7 +7,8 @@ const { getPlaylist, getFullPlaylist } = require("../spotify");
 
 const run = async () => {
   // const playlist = await getFullPlaylist("2WfP3mGUpywvXYy21lnGJq"); // 2021
-  const playlist = await getFullPlaylist("1lwtTvyFZLJONjzGxCclCn");
+  // const playlist = await getFullPlaylist("1lwtTvyFZLJONjzGxCclCn"); // 2022
+  const playlist = await getFullPlaylist("3Upy9YJeZSbVUZiCYZy66D"); // 2023
 
   const tracks = playlist.tracks.items.map(({ track }, idx, arr) => ({
     spotifyTrack: `https://open.spotify.com/track/${track.id}`,
@@ -16,7 +17,7 @@ const run = async () => {
     position: arr.length - idx,
   }));
 
-  await runUploadServer(tracks, SOURCE.BIG_J_THE_WIZARD_KING, 2022);
+  await runUploadServer(tracks, SOURCE.BIG_J_THE_WIZARD_KING, 2023);
   // runUpload(songs, SOURCE.PITCHFORK);
 };
 
